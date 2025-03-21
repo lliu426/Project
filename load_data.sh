f86c0295a041493d2f34ebc881c63e0148753a88
Lexiang Liu <lliu426@wisc.edu>
2025-03-20 19:39:17 -0500

    data loaded

---
+++ load_data.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl -L -o indian-5000-cities-weather-data.zip \
+https://www.kaggle.com/api/v1/datasets/download/mukeshdevrath007/indian-5000-cities-weather-data
+unzip indian-5000-cities-weather-data.zip
